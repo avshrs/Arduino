86ca4149c6dcd891a0c7503c2b130c7700737697
Sebastian <sebastian.antosiak@gmail.com>
2018-11-28 21:32:44 +0100

    Update arduino-mcp23017.cpp

--- Arduino-nano/arduino-mcp23017.cpp
+++ Arduino-nano/arduino-mcp23017.cpp
@@ -1,1 +1,218 @@
- 
+#include <RBD_SerialManager.h>
+#include <stdint.h>
+#include <Wire.h>
+RBD::SerialManager serial_manager;
+
+uint8_t MCPMa = 0;
+uint8_t MCPMb = 1;
+uint8_t MCPFa = 2;
+uint8_t MCPFb = 3;
+uint8_t MCPa  = 0;
+uint8_t MCPb  = 1;
+uint8_t zero = 0;
+uint8_t one  = 1;
+uint8_t MCP1_ADDR  = 0x20;
+uint8_t MCP2_ADDR  = 0x21;
+uint8_t MCP_A_ADDR = 0x12; // Register Address of Port A
+uint8_t MCP_B_ADDR = 0x13; // Register Address of Port B
+uint8_t MCP_INIT   = 0x00; // MCP init
+uint8_t MCP_TO_OUT = 0x00; // Register Address of Port A
+uint8_t MCP_TO_IN  = 0x01; // Register Address of Port A
+
+
+int delay_v = 10;   //refresh timer 10 ms
+int delay_mili_micro = 1;
+
+union uMCP1{
+    uint32_t MCP1p;     
+    uint8_t  MCP1[4]; 
+}; 
+uMCP1 MCP1u = {0};
+
+union uMCP2{
+    uint32_t MCP2p;     
+    uint8_t  MCP2[4]; 
+};           
+uMCP2 MCP2u = {0};
+
+
+
+void print_binary(uint32_t &v)
+{  
+    for (uint8_t i = 0 ; i< sizeof(v)*8 ; ++i){
+       if ((v & (1 << i )) > 0) 
+           Serial.print("1");
+       else
+           Serial.print("0"); 
+        Serial.println(" "); 
+    }
+}
+
+void printAll(uint8_t &HUMAN,uint32_t &MEMORY){
+    if (HUMAN == true ){
+        for (uint8_t i = 0 ; i< sizeof(MEMORY)*8 ; ++i){
+            Serial.print("Output nr: ");
+            if ((MEMORY & (1 << i )) > 0) 
+                Serial.print("1");
+            else
+                Serial.print("0"); 
+        }
+    }
+    else
+    print_binary(MEMORY);
+}
+
+void stateMcpToOn(uint8_t &I2C_ADDR,uint8_t PIN, uint8_t &MEMORY, uint8_t &FORCED,uint8_t SIDE, bool FORCE){
+    uint8_t mask = (1 << PIN);
+    if((MEMORY & mask) != mask){
+        MEMORY |= mask;
+        if(FORCE)
+            FORCED ^= mask;
+        Wire.beginTransmission(I2C_ADDR);
+        if (SIDE == MCPa){
+            Wire.write(MCP_A_ADDR);
+            Wire.write(MEMORY);
+            Serial.println(MEMORY);   
+        }
+        if (SIDE == MCPb){
+            Wire.write(MCP_B_ADDR);
+            Wire.write(MEMORY);  
+            Serial.println(MEMORY);    
+        }
+        Wire.endTransmission();    
+    }
+ }
+
+
+
+
+void getMcpValue(uint8_t &I2C_ADDR,uint8_t PIN, uint8_t &MEMORY, uint8_t &FORCED,uint8_t SIDE){
+    uint8_t value = 0;
+    Wire.beginTransmission(I2C_ADDR);
+    if (SIDE == MCPa){
+        Wire.write(MCP_A_ADDR);
+        Wire.requestFrom((int)I2C_ADDR, 1);
+        while(Wire.available()) {   
+            value = Wire.read();
+        } 
+    }
+    if (SIDE == MCPb){
+        Wire.write(MCP_B_ADDR);
+        Wire.requestFrom((int)I2C_ADDR, 1);
+        while(Wire.available()) {   
+            value = Wire.read();
+        }       
+    }
+    Wire.endTransmission();    
+    uint8_t mask = (1 << PIN);
+    if (( FORCED & mask) != mask){
+      if (( MEMORY & mask) > (value & mask))
+            MEMORY |= mask;
+      if (( MEMORY & mask) < (value & mask))
+            MEMORY &= ~mask;
+    }
+}
+
+void stateMcpToOff(uint8_t &I2C_ADDR, uint8_t PIN, uint8_t &MEMORY, uint8_t &FORCED,uint8_t SIDE, bool FORCE){
+        
+        uint8_t mask = (1 << PIN);
+        if ((MEMORY &   mask) == mask){
+             MEMORY &= ~mask;
+            if(FORCE)
+                FORCED ^= mask;
+            Wire.beginTransmission(I2C_ADDR);
+            if (SIDE == MCPa){
+                Wire.write(MCP_A_ADDR);
+                Wire.write(MEMORY);     
+            }
+            if (SIDE == MCPb){
+                Wire.write(MCP_B_ADDR);
+                Wire.write(MEMORY);   
+            }
+            Wire.endTransmission();    
+        }
+       
+ }
+
+
+void serialCom(){
+    if(serial_manager.onReceive()) {
+        
+        if(serial_manager.isCmd("test")) {
+            serial_manager.println("True");
+        }
+        if(serial_manager.isCmd("delay")) {
+            delay_v = serial_manager.getParam().toInt();
+        }
+        if(serial_manager.isCmd("delay_mu")) {
+            delay_mili_micro = serial_manager.getParam().toInt();
+        }
+
+        if(serial_manager.isCmd("gMCP1")) {
+            printAll(zero,MCP1u.MCP1p);
+           
+        }
+        
+        if(serial_manager.isCmd("gMCP2")) {
+             printAll(one,MCP1u.MCP1p);
+        }
+        
+        
+       
+
+    }
+}
+
+void initMcp(uint8_t &MCP_ADDR, uint8_t &MCP_INIT,uint8_t &MCP_A,uint8_t &MCP_B ){   
+    Wire.begin();
+    Wire.beginTransmission(MCP_ADDR);
+    Wire.write(MCP_INIT);
+    Wire.write(MCP_A);
+    Wire.write(MCP_B);
+    Wire.endTransmission();
+}
+
+void setup() {
+    serial_manager.start();
+    initMcp(MCP1_ADDR, MCP_INIT, MCP_TO_OUT, MCP_TO_OUT);
+    initMcp(MCP2_ADDR, MCP_INIT, MCP_TO_OUT, MCP_TO_OUT);
+}
+
+
+
+
+
+
+void loop() {
+    serialCom();
+    for(uint8_t i=0; i<16;++i){  
+        stateMcpToOn(MCP2_ADDR,i,MCP2u.MCP2[MCPMa],MCP2u.MCP2[MCPFa],MCPa,true);    
+        serialCom();
+        if (delay_mili_micro > 0)
+             delay(delay_v);    
+        else
+             delayMicroseconds(delay_v);      
+        stateMcpToOff(MCP2_ADDR,i,MCP2u.MCP2[MCPMa],MCP2u.MCP2[MCPFa],MCPa,true);
+        serialCom();
+        if (delay_mili_micro > 0)
+             delay(delay_v);    
+        else
+             delayMicroseconds(delay_v);       
+        stateMcpToOn(MCP1_ADDR,i,MCP1u.MCP1[MCPMb],MCP1u.MCP1[MCPFb],MCPb,true);    
+        serialCom();
+        if (delay_mili_micro > 0)
+            delay(delay_v);    
+        else
+            delayMicroseconds(delay_v);      
+        stateMcpToOff(MCP1_ADDR,i,MCP1u.MCP1[MCPMb],MCP1u.MCP1[MCPFb],MCPb,true);
+        serialCom();
+        if (delay_mili_micro > 0)
+             delay(delay_v);    
+        else
+             delayMicroseconds(delay_v);       
+   
+     }    
+}
+
+
+
