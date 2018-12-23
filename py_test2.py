285b889ae2a8f2fa3ba49d1afbec82da96981511
avshrs <avshrs@gmail.com>
2018-12-23 20:38:16 +0100

    update

--- py_test2.py
+++ py_test2.py
@@ -25,21 +25,3 @@ for i in range(0,8):
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
     sock.sendto(frame_be , (UDP_IP, UDP_PORT))
-"""
-for i in range(7,-1,-1):
-    #time.sleep(0.2)
-    HEAD = 0xb1  # 10110001  #0xA0 read value from side = A /0xB0 read value from side = b //0xA1 write walue A //0xB1 write walue  B
-    HEAD_ = '{:08b}'.format(HEAD)
-    MCPADDRESS = 0x20  # //sensor number i2c adres eg 0x20, 0x21 to 0x27
-    MCPADDRESS_  = '{:08b}'.format(MCPADDRESS)
-    PIN = i
-    PIN_ = '{:08b}'.format(PIN)
-    RW = 0
-    RW_ = '{:08b}'.format(RW)
-    HEAD_ = '{:08b}'.format(HEAD)
-    #frame = int(HEAD_+MCPADDRESS_[::-1]+PIN_[::-1]+RW_[::-1], 2)
-    frame = int(HEAD_+MCPADDRESS_+PIN_+RW_, 2)
-    frame_be = struct.pack('>I', frame)
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-    sock.sendto(frame_be , (UDP_IP, UDP_PORT))"""
